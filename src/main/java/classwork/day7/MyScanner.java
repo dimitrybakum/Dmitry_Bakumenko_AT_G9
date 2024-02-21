1a964c85deac6f0783ff3189840dad5386e88def
d.bakumenko <d.bakumenko@gmail.com>
2024-02-21 13:49:06 +0300

    day7 class work

---
+++ src/main/java/classwork/day7/MyScanner.java
@@ -0,0 +1,18 @@
+package classwork.day7;
+
+import java.util.Scanner;
+
+public class MyScanner {
+  public static void main(String[] args) {
+
+    Scanner myScanner = new Scanner(System.in);
+
+    while (myScanner.hasNext()) {
+      String incoming = myScanner.nextLine();
+      if (incoming.equalsIgnoreCase("stop")) {
+        return;
+      }
+      System.out.printf("Just got '%s", incoming).println();
+    }
+  }
+}
