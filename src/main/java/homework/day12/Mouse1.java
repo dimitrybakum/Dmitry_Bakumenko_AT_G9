9880d0d74e7ea9a64ad3ea116bc40b6cf36a4ea3
d.bakumenko <d.bakumenko@gmail.com>
2024-03-11 16:28:43 +0300

    day12_homework_Threads_remove_commented lines

--- src/main/java/homework/day12/Mouse1.java
+++ src/main/java/homework/day12/Mouse1.java
@@ -5,20 +5,11 @@ import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Mouse1 {
-  // private String pattern - содержит паттерн имени мыши формате "Mouse <n>", где n - порядковый
-  // номер мыши
   private String pattern = "Mouse ";
-
-  // private String name - имя мыши в формате "Mouse <n>", где n - порядковый номер мыши
   private String name;
-
-  // конструктор public Mouse(int number) - в нем инициализируется поле name
   public Mouse1(int number) {
     this.name = pattern + number;
   }
-
-  // метод public void peep() - печатает в консоль фразу в формате "Mouse <n> PEEP!", где n -
-  // порядковый номер мыши, и заставляет поток заснуть на 200мс
   public void peep() throws InterruptedException {
     System.out.println(name + " PEEP!");
     Thread.sleep(200);
